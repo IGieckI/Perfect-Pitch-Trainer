9aa72840bc6379b2e3c7503c63a2f8f2cd7b73b7
IGieckI <giacomo.antonelli3@gmail.com>
2024-02-27 00:58:37 +0100

    Implementation of a Tone example 🔥

---
+++ client/src/components/ToneComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="playSound">Play Sound</button>
+  </div>
+</template>
+
+<script>
+import * as Tone from 'tone';
+
+export default {
+  methods: {
+    playSound() {
+      const sampler = new Tone.Sampler({
+        urls: {
+          "C4": "C4.mp3",
+          "D#4": "Ds4.mp3",
+          "F#4": "Fs4.mp3",
+          "A4": "A4.mp3",
+        },
+        release: 1,
+        baseUrl: "https://tonejs.github.io/audio/salamander/",
+      }).toDestination();
+
+      Tone.loaded().then(() => {
+        sampler.triggerAttackRelease(["Eb4", "G4", "Bb4"], 10);
+      })
+    }
+  }
+}
+</script>
