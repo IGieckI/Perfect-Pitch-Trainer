f23bd842aedcda7b8d8fe6e630accc8622f2f77e
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-02-28 15:25:49 +0100

    Configure client side and create navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Perfect Pitch Trainer</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <!--
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                -->
+                
+            </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss">
+$text-color: #39db6f;
+
+.navbar {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.418);
+    
+    a {
+        color: $text-color;
+        font-weight: 500;
+    }
+}
+
+</style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createRouter, createWebHistory, Router } from 'vue-router'
+import axios from 'axios'
+import './styles/style.scss'
+import 'bootstrap'
 import App from './App.vue'
 
 createApp(App).mount('#app')
