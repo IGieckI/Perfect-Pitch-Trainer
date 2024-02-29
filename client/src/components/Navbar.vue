a998fda0ced40043eff671ea0f589631e00ec268
IGieckI <giacomo.antonelli3@gmail.com>
2024-02-29 22:33:09 +0100

    Improve navbar css

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -7,35 +7,24 @@
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Perfect Pitch Trainer</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Home</a>
-                </li>
-                <!--
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
                     </li>
-                -->
-                
-            </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Infinite</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Sandbox</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Stats</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
 </template>
-
-<style lang="scss">
-$text-color: #39db6f;
-
-.navbar {
-    border-bottom: 1px solid rgba(0, 0, 0, 0.418);
-    
-    a {
-        color: $text-color;
-        font-weight: 500;
-    }
-}
-
-</style>
