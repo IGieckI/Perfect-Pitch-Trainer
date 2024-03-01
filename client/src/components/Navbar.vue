294805f41ea2f9c7680c1b3877a59ae4e668c188
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-01 01:47:48 +0100

    Update Navbar.vue

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -5,23 +5,23 @@
 <template>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Perfect Pitch Trainer</a>
+            <router-link class="navbar-brand" to="/">Perfect Pitch Trainer</router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item me-5">
-                        <a class="nav-link" aria-current="page" href="#">Infinite</a>
+                        <router-link class="nav-link" to="/infinite">Infinite</router-link>
                     </li>
                     <li class="nav-item me-5">
-                        <a class="nav-link" aria-current="page" href="#">Set Of Notes</a>
+                        <router-link class="nav-link" to="/set-of-notes">Set Of Notes</router-link>
                     </li>
                     <li class="nav-item me-5">
-                        <a class="nav-link" aria-current="page" href="#">Sandbox</a>
+                        <router-link class="nav-link" to="/sandbox">Sandbox</router-link>
                     </li>
                     <li class="nav-item me-5">
-                        <a class="nav-link" aria-current="page" href="#">Stats</a>
+                        <router-link class="nav-link" to="/stats">Stats</router-link>
                     </li>
                 </ul>
             </div>
