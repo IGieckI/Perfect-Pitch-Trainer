4d3dd9ef6d4f78108c93fc9e5433fa3e919b37dd
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-04 01:41:51 +0100

    Fix Piano typescript code (CSS unstable)

--- client/src/components/Piano.vue
+++ client/src/components/Piano.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="piano">
-        <div v-for="(note, index) in notes" :key="index" class="key" :class="{'black-key': isBlackKey(note), 'white-key': !isBlackKey(note)}"></div>
+        <div v-for="(note, index) in notes" :key="index" class="key" :class="{ 'black-key': isBlackKey(note), 'white-key': !isBlackKey(note) }" @mousedown="playNote(note)" @mouseup="stopNote"></div>
     </div>
 </template>
 
@@ -9,59 +9,56 @@ import { defineComponent } from 'vue';
 import * as Tone from 'tone';
 
 export default defineComponent({
-    data() {
-        return {
-            synth: new Tone.Synth().toDestination(),
-            notes: ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5']
-        }
+    name: 'PlayablePiano',
+    setup() {
+        const notes = [
+        'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
+        'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5'
+    ];
+
+    const synth = new Tone.Synth().toDestination();
+
+    const playNote = (note: string) => {
+        synth.triggerAttack(note);
+    };
+
+    const stopNote = () => {
+        synth.triggerRelease();
+    };
+
+    const isBlackKey = (note: string) => note.includes('#');
+
+    return { notes, playNote, stopNote, isBlackKey };
     },
-    methods: {
-        isBlackKey(note: string) {
-            return note.includes('#');
-        },
-        playNote(note: string) {
-            Tone.context.resume().then(() => {
-                this.synth.triggerAttackRelease(note, '8n');
-            });
-        }
-    }
 });
 </script>
 
+<style lang="scss">
+.piano {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
+.key {
+    cursor: pointer;
+    border: 1px solid black;
+    height: 150px;
+    width: 40px;
+    margin: 1px;
 
-<style scoped lang="scss">
-    .piano {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    &.black-key {
+    background-color: black;
+    width: 30px;
+    margin: 0;
+    position: relative;
+    left: -15px;
+    z-index: 1;
+    height: 100px;
     }
 
-    .key {
-        border: 1px solid #000;
-        height: 150px;
-        width: 40px;
-        cursor: pointer;
-        &:hover {
-        background-color: #f0f0f0;
-        }
+    &.white-key {
+    background-color: white;
     }
-
-    .black-key {
-        background-color: #000;
-        position: absolute;
-        margin: 0;
-        color: #fff;
-        width: 30px;
-        position: relative;
-        top: -25px;
-        left: -15px;
-        z-index: 1;
-        height: 100px;
-    }
-
-    .white-key {
-        background-color: #fff;
-        margin: -1px;
-    }
+}
 </style>
