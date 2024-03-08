c086be299206505b80f5fe7d33ec4d745fc9e0b4
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-08 18:53:01 +0100

    Add the SetOfNotes page

---
+++ client/src/views/SetOfNotes.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="setup" >
+        <SetupTest />
+    </div>
+    <div class="container-fluid text-center">
+        <div class="row">
+            <Saxophone />
+        </div>
+        <div class="row">
+            <Piano />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Piano from '../components/Piano.vue';
+import Saxophone from '../components/Saxophone.vue';
+import SetupTest from '../components/SetupTest.vue';
+
+export default defineComponent({
+    data() {
+        return {};
+    },
+    methods: {},
+    mounted() {
+    },
+    components: { Piano, Saxophone, SetupTest }
+})
+</script>
