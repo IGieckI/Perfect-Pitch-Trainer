ea97fcd4a2a23dd44d60b5220280f6257f686276
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-08 18:52:06 +0100

    Add Saxophone vue page

---
+++ client/src/components/Saxophone.vue
@@ -0,0 +1,29 @@
+<template>
+    <img id="saxophone" width="377" height="364" alt="Saxophone" src="../assets/saxophone.svg" @click="bounce" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Saxophone',
+    methods: {
+        bounce() {
+            const saxophone = document.getElementById('saxophone');
+            if (saxophone) {
+                saxophone.style.animation = 'bounce 0.5s';
+                setTimeout(() => {
+                    saxophone.style.animation = '';
+                }, 500);
+            }
+        }
+    },
+});
+
+// This guy should bounce when clicked
+
+</script>
+
+<style lang="scss">
+
+</style>
