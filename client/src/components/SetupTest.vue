f7f1869194e1c545c7782156cd90ed672f10ee7c
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-11 17:00:29 +0100

    Improve SetupTest.vue

--- client/src/components/SetupTest.vue
+++ client/src/components/SetupTest.vue
@@ -1,38 +1,18 @@
 <template>
-  <div class="container mt-5">
-    <div class="card">
-      <h5 class="card-header text-center title-text">SETUP TEST</h5>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-md-6">
-            <ul class="list-unstyled">
-              <li class="mb-2"><input type="checkbox" id="normal-notes" name="normal-notes" value="normal-notes" class="round-checkbox"><label for="normal-notes">Normal Notes</label></li>
-              <li class="mb-2"><input type="checkbox" id="minor-chords" name="minor-chords" value="minor-chords" class="round-checkbox"><label for="minor-chords">Minor Chords</label></li>
-              <li class="mb-2"><input type="checkbox" id="seventh-chords" name="seventh-chords" value="seventh-chords" class="round-checkbox"><label for="seventh-chords">Seventh Chords</label></li>
-              <li class="mb-2"><input type="checkbox" id="extended-chords" name="extended-chords" value="extended-chords" class="round-checkbox"><label for="extended-chords">Extended Chords</label></li>
-            </ul>
-          </div>
-          <div class="col-md-6">
-            <ul class="list-unstyled">
-              <li class="mb-2"><input type="checkbox" id="major-chords" name="major-chords" value="major-chords" class="round-checkbox"><label for="major-chords">Major Chords</label></li>
-              <li class="mb-2"><input type="checkbox" id="augmented-chords" name="augmented-chords" value="augmented-chords" class="round-checkbox"><label for="augmented-chords">Augmented Chords</label></li>
-              <li class="mb-2"><input type="checkbox" id="diminished-chords" name="diminished-chords" value="diminished-chords" class="round-checkbox"><label for="diminished-chords">Diminished Chords</label></li>
-              <li class="mb-2"><input type="checkbox" id="chords-inversions" name="chords-inversions" value="chords-inversions" class="round-checkbox"><label for="chords-inversions">Chords Inversions</label></li>
-            </ul>
-          </div>
-          <div class="row mt-3">
-            <div class="col-md-6 offset-md-3 mt-3">
-              <div class="form-group d-flex align-items-center">
-                <label for="exercises-number" class="mr-3">Exercises number:</label>
-                <input type="text" id="exercises-number" class="form-control narrower-input" placeholder="Enter number of exercises">
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 offset-md-3 mt-3">
-            <button type="button" class="button btn btn-primary btn-block ">Confirm</button>
-          </div>
-        </div>
+  <div class="wrapper text-dark">
+    <h3 class="text-center fw-bold">Setup Test</h3>
+    <form class="ms-4" id="notesConfigurationForm">
+      <div class="form-check" v-for="(item, index) in items" :key="index">
+        <input type="radio" :id="item" class="form-check-input" v-model="selectedItem" :value="item">
+        <label class="form-check-label" :for="item">{{ item }}</label>
       </div>
+      <div class="mt-3">
+        <label for="exerciseNumber" class="fw-semibold">Exercise number: </label>
+        <input type="number" id="exerciseNumber" class="form-control fs-5" v-model="exerciseNumber">
+      </div>
+    </form>
+    <div class="text-center mt-4 mb-3" form="notesConfigurationForm">
+      <button type="submit" class="btn fs-5" id="setupTestSubmitButton">Confirm</button>
     </div>
   </div>
 </template>
@@ -41,47 +21,47 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'SetupTest',
-});
+  data() {
+    return {
+      items: ['Normal Notes', 'Minor Chords', 'Seventh Chords', 'Extended Chords', 'Major Chords', 'Augmented Chords', 'Diminished Chords', 'Chords Inversions'],
+      selectedItem: null,
+      exerciseNumber: 1
+    }
+  }
+})
 </script>
 
 <style lang="scss">
-  .round-checkbox {
-    appearance: none;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    border: 2px solid black;
-    outline: none;
-    cursor: pointer;
-    vertical-align: middle;
-    position: relative;
-    margin-right: 8px;
-  }
-  
-  .round-checkbox:checked::after {
-    content: '';
-    width: 8px;
-    height: 8px;
-    background: black;
-    border-radius: 50%;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+$wrapper-background-color: #ffffff;
+$wrapper-border-color: #000000;
+$submit-button-color: #570000;
+.wrapper {
+  width: 30%;
+  background-color: $wrapper-background-color;
+  border: 1px solid $wrapper-border-color;
+  border-radius: 10px;
+  margin: auto;
+  font-size: 22px;
+  font-weight: 500;
+
+  form {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+
+    .form-check-input {
+      border: 2px solid black;
+    }
+
   }
 
-  .narrower-input {
-    width: 230px; /* Adjust width as needed */
-  }
+  #setupTestSubmitButton {
+    background-color: $submit-button-color;
+    color: white;
 
-  .button {
-    background-color: #570000
+    &:hover {
+      background-color: lighten($submit-button-color, 10%);
+    }
   }
-
-  a {
-    font-weight: bold;
-  }
+}
 </style>
