9aaefb59ff15c556195eaf63ccaac68f98ca0494
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-12 12:10:40 +0100

    Made the add of new set of notes highly dynamic

--- client/src/views/SetOfNotes.vue
+++ client/src/views/SetOfNotes.vue
@@ -19,10 +19,11 @@ import SetupTest from '../components/SetupTest.vue';
 
 export default defineComponent({
     data() {
-        return {            
-            normal_notes: [['C1'], ['D1'], ['E1'], ['F1'], ['G1'], ['A1'], ['B1'], ['C2'], ['D2'], ['E2'], ['F2'], ['G2'], ['A2'], ['B2'], ['C3'], ['D3'], ['E3'], ['F3'], ['G3'], ['A3'], ['B3'], ['C4'], ['D4'], ['E4'], ['F4'], ['G4'], ['A4'], ['B4'], ['C5'], ['D5'], ['E5'], ['F5'], ['G5'], ['A5'], ['B5'], ['C6'], ['D6'], ['E6'], ['F6'], ['G6'], ['A6'], ['B6'], ['C7'], ['D7'], ['E7'], ['F7'], ['G7'], ['A7'], ['B7'], ['C8']],
-            minor_chords: [['C1', 'Eb', 'G'], ['C#', 'E', 'G#'], ['Db', 'Fb', 'Ab'], ['D', 'F', 'A'], ['D#', 'F#', 'A#'], ['Eb', 'Gb', 'Bb'], ['E', 'G', 'B'], ['F', 'Ab', 'C'], ['F#', 'A', 'C#'], ['Gb', 'A', 'Db'], ['G', 'Bb', 'D'], ['G#', 'B', 'D#'], ['Ab', 'Cb', 'Eb'], ['A', 'C', 'E'], ['A#', 'C#', 'E#'], ['Bb', 'Db', 'F'], ['B', 'D', 'F#']],
-            
+        return {
+            notes: {
+                "normal notes": [['C1'], ['D1'], ['E1'], ['F1'], ['G1'], ['A1'], ['B1'], ['C2'], ['D2'], ['E2'], ['F2'], ['G2'], ['A2'], ['B2'], ['C3'], ['D3'], ['E3'], ['F3'], ['G3'], ['A3'], ['B3'], ['C4'], ['D4'], ['E4'], ['F4'], ['G4'], ['A4'], ['B4'], ['C5'], ['D5'], ['E5'], ['F5'], ['G5'], ['A5'], ['B5'], ['C6'], ['D6'], ['E6'], ['F6'], ['G6'], ['A6'], ['B6'], ['C7'], ['D7'], ['E7'], ['F7'], ['G7'], ['A7'], ['B7'], ['C8']],
+                "minor chords": [['C1', 'Eb', 'G'], ['C#', 'E', 'G#'], ['Db', 'Fb', 'Ab'], ['D', 'F', 'A'], ['D#', 'F#', 'A#'], ['Eb', 'Gb', 'Bb'], ['E', 'G', 'B'], ['F', 'Ab', 'C'], ['F#', 'A', 'C#'], ['Gb', 'A', 'Db'], ['G', 'Bb', 'D'], ['G#', 'B', 'D#'], ['Ab', 'Cb', 'Eb'], ['A', 'C', 'E'], ['A#', 'C#', 'E#'], ['Bb', 'Db', 'F'], ['B', 'D', 'F#']]
+            },
             setupComplete: false,
             totalNotes: [] as String[][][]
         };
@@ -51,15 +52,10 @@ export default defineComponent({
         },
 
         setFilters(selectedItems: String[]) {
-            this.setupComplete = true
-            console.log(selectedItems)
+            this.setupComplete = true;
             
-            if (selectedItems.includes("Normal Notes")) {
-                this.totalNotes.push(this.normal_notes);
-            }
-            
-            if (selectedItems.includes("Minor Chords")) {
-                this.totalNotes.push(this.minor_chords);
+            for (let i = 0; i < selectedItems.length; i++) {
+                this.totalNotes.push(this.notes[selectedItems[i].toLowerCase()]);
             }
         }
     },
