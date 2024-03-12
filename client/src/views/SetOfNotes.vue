241e3a089e04f9c749ad0fdd811569fcaa80bb28
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-12 16:38:57 +0100

    Satisfied with the whims of Typescript

--- client/src/views/SetOfNotes.vue
+++ client/src/views/SetOfNotes.vue
@@ -23,23 +23,23 @@ export default defineComponent({
             notes: {
                 "normal notes": [['C1'], ['D1'], ['E1'], ['F1'], ['G1'], ['A1'], ['B1'], ['C2'], ['D2'], ['E2'], ['F2'], ['G2'], ['A2'], ['B2'], ['C3'], ['D3'], ['E3'], ['F3'], ['G3'], ['A3'], ['B3'], ['C4'], ['D4'], ['E4'], ['F4'], ['G4'], ['A4'], ['B4'], ['C5'], ['D5'], ['E5'], ['F5'], ['G5'], ['A5'], ['B5'], ['C6'], ['D6'], ['E6'], ['F6'], ['G6'], ['A6'], ['B6'], ['C7'], ['D7'], ['E7'], ['F7'], ['G7'], ['A7'], ['B7'], ['C8']],
                 "minor chords": [['C1', 'Eb', 'G'], ['C#', 'E', 'G#'], ['Db', 'Fb', 'Ab'], ['D', 'F', 'A'], ['D#', 'F#', 'A#'], ['Eb', 'Gb', 'Bb'], ['E', 'G', 'B'], ['F', 'Ab', 'C'], ['F#', 'A', 'C#'], ['Gb', 'A', 'Db'], ['G', 'Bb', 'D'], ['G#', 'B', 'D#'], ['Ab', 'Cb', 'Eb'], ['A', 'C', 'E'], ['A#', 'C#', 'E#'], ['Bb', 'Db', 'F'], ['B', 'D', 'F#']]
-            },
+            } as { [key: string]: string[][] },
             setupComplete: false,
-            totalNotes: [] as String[][][]
+            totalNotes: [] as string[][],
+            exerciseNumber: 0,
         };
     },
     methods: {
         playSound() {
 
-            const random_note = this.totalNotes[Math.floor(Math.random() * this.totalNotes.length)];            
+            const random_note: string[] = this.totalNotes[Math.floor(Math.random() * this.totalNotes.length)];            
 
-            const note_urls: { [key: string]: string } = {}; // Define note_urls as an empty object            
+            const note_urls: { [key: string]: string } = {};
 
             for (let i = 0; i < random_note.length; i++) {
-                let note_url = random_note[i]; // Extract the note URL
-                console.log(note_url);
-                note_url = note_url.replace('#', 's'); // Replace '#' with 's'
-                note_urls[random_note[i]] = note_url + '.mp3'; // Assign the modified note URL to note_urls
+                let note_url: string = random_note[i];
+                note_url = note_url.replace('#', 's');
+                note_urls[random_note[i]] = note_url + '.mp3';
             }
             console.log(note_urls);
 
@@ -62,11 +62,15 @@ export default defineComponent({
             })
         },
 
-        setFilters(selectedItems: String[]) {
-            this.setupComplete = true;
-            
+        setFilters(selectedItems: string[], exerciseNumber: number) {
+            this.setupComplete = true;            
+            this.exerciseNumber = exerciseNumber;
+
+            console.log("Tipo:" + typeof(selectedItems[0].toLowerCase()))
+
             for (let i = 0; i < selectedItems.length; i++) {
-                for (let j = 0; j < this.notes[selectedItems[i].toLowerCase()].length; j++) {
+                let notes: string[][] = this.notes["normal notes"]
+                for (let j = 0; j < notes.length; j++) {
                     this.totalNotes.push(this.notes[selectedItems[i].toLowerCase()][j]);
                 }
             }
