b91c56c22146b2bcee85df67723c7423f13baff8
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-15 00:10:05 +0100

    Create of Sandbox view

---
+++ client/src/views/Sandbox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
+                <Piano class="mt-4" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Piano from '../components/Piano.vue';
+
+export default defineComponent({
+    components: { Piano }
+})
+</script>
