ff34c693bba993623e84f0a494c77b20de342f3c
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-15 00:09:33 +0100

    Add new routes to the main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,12 +6,16 @@ import 'bootstrap'
 import App from './App.vue'
 import Home from './views/Home.vue'
 import SetOfNotes from './views/SetOfNotes.vue'
+import Infinite from './views/Infinite.vue'
+import Sandbox from './views/Sandbox.vue'
 
 const router: Router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Home},
         { path: '/set-of-notes', component: SetOfNotes},
+        { path: '/infinite', component: Infinite},
+        { path: '/sandbox', component: Sandbox},
     ]
 })
 
