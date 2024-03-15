4d26c1251b859177f54c1d016437236036e7768f
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-15 17:22:51 +0100

    Fix TogglePiano, fix .env problems on backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,17 @@
 import express, { Express } from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import history from 'connect-history-api-fallback';
 import gameRouter from './routes/games_router'
 import statsRouter from './routes/stats_router'
 
+dotenv.config();
+let port: number = Number(process.env.SERVER_PORT);
+
 const app: Express = express();
 
 app.use(bodyParser.json())
-
-// Not sure the load works like this
-app.use(express.static(path.join(__dirname, '../../client/src')));
 
 app.use(gameRouter)
 app.use(statsRouter)
@@ -22,6 +23,6 @@ app.use(function(req, res, next) {
     res.status(404).send("Ops... Page not found!");
 })
 
-app.listen(process.env.SERVER_PORT, function() {
-    console.log(`Listening on http://localhost:${process.env.SERVER_PORT}`);
+app.listen(port, function() {
+    console.log(`Listening on http://localhost:${port}`);
 })
