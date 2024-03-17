b82cf35b1cbe2eeb962d759aaa2cf23f55eb007c
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-17 17:16:48 +0100

    Add HomeContainer.vue UI component and add Home.vue UI

---
+++ client/src/components/HomeContainer.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="home-container text-dark d-flex flex-column">
+        <h3 class="text-center fw-bolder fs-2"><slot name="title">Home Container Title</slot></h3>
+        <div class="mt-3 text-left fs-3 ps-3 fw-bold">
+            <slot name="content">Home Container Content.</slot>
+        </div>
+        <div class="text-center mt-4 mb-3">
+            <button class="btn fw-bold" @click="handleClick">
+                <slot name="action"></slot>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'HomeContainer',
+    data() {
+
+    },
+    methods: {
+        handleClick() {
+            this.$emit('button-clicked');
+        },
+
+        handleHover() {
+            
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+$wrapper-background-color: #dddddd;
+$wrapper-hover-background-color: #ffffff;
+$wrapper-border-color: #000000;
+$submit-button-text-color: #ffffff;
+$submit-button-color: #570000;
+
+.home-container {
+    background-color: $wrapper-background-color;
+    border: 2px solid $wrapper-border-color;
+    border-radius: 20px;
+    width: 25%;
+    height: auto;
+    font-size: 22px;
+    transition: transform 0.3s ease-in-out;
+    &:hover {
+        transform: translateY(-10px);
+        background-color: $wrapper-hover-background-color;
+        transition: 0.7s ease-in-out;
+    }
+
+    .btn {
+        background-color: $submit-button-color;
+        color: $submit-button-text-color;
+        font-size: 23px;
+        width: 30%;
+
+        &:hover {
+            background-color: lighten($submit-button-color, 10%);
+        }
+    }
+
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,79 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
-                <h1> Welcome to Perfect Pitch Trainer, please select one of the gamemodes above.</h1>
+            <div class="col-12 text-center">
+                <h2 class="title mt-4">Welcome to Perfect Pitch Trainer! 🎹<br>Select one of the following options to start</h2>
             </div>
+        </div>
+        <div class="container-home mt-5">
+            <HomeContainer class="container-home-item" @button-clicked="handleActionClicked">
+                <template #title>Infinite</template>
+                <template #content>Best score goes here</template>
+                <template #action>Start</template>
+            </HomeContainer>
+            <HomeContainer class="container-home-item" @button-clicked="handleActionClicked">
+                <template #title>Set Of Notes</template>
+                <template #content>Correct Guess Percentage: 100%</template>
+                <template #action>Start</template>
+            </HomeContainer>
+            <HomeContainer class="container-home-item" @button-clicked="handleActionClicked">
+                <template #title>Sandbox</template>
+                <template #content>Practice in Sandbox mode</template>
+                <template #action>Start</template>
+            </HomeContainer>
+            <HomeContainer class="container-home-item" @button-clicked="handleActionClicked">
+                <template #title>Stats</template>
+                <template #content>See your statistics</template>
+                <template #action>See</template>
+            </HomeContainer>
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import HomeContainer from '../components/HomeContainer.vue';
 
-export default defineComponent({})
+export default defineComponent({
+    components: { HomeContainer },
+    data() {
+
+    },
+    methods: {
+        handleActionClicked() {
+            console.log(`Button clicked in component!`)
+        }
+    }
+
+})
 </script>
+
+<style lang="scss">
+$title-color: #ffffff;
+$title-stroke-color: #000000;
+
+.title {
+    color: $title-color;
+    font-size: 50px;
+}
+
+.container-home {
+    width: 100%;
+    margin: auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+    row-gap: 30px;
+    .container-home-item {
+        width: 50%;
+    }
+
+    @media (max-width: 906px) {
+        .container-home-item {
+            width: 70%;
+        }
+        grid-template-columns: 1fr;
+    }
+}
+
+</style>
