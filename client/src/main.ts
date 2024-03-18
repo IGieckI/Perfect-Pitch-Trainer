73901ad48f0033e3c1f24b74e7ea1663ea6677a7
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-18 16:43:57 +0100

    Add stats vue and routing

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory, Router } from 'vue-router'
-import axios from 'axios'
 import './styles/style.scss'
 import 'bootstrap'
 import App from './App.vue'
@@ -8,14 +7,16 @@ import Home from './views/Home.vue'
 import SetOfNotes from './views/SetOfNotes.vue'
 import Infinite from './views/Infinite.vue'
 import Sandbox from './views/Sandbox.vue'
+import Stats from './views/Stats.vue'
 
 const router: Router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Home},
-        { path: '/set-of-notes', component: SetOfNotes},
-        { path: '/infinite', component: Infinite},
-        { path: '/sandbox', component: Sandbox},
+        { path: '/', component: Home },
+        { path: '/set-of-notes', component: SetOfNotes },
+        { path: '/infinite', component: Infinite },
+        { path: '/sandbox', component: Sandbox },
+        { path: '/stats', component: Stats }
     ]
 })
 
---
+++ client/src/views/Stats.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Here goes the stats section</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+})
+</script>
+
+<style scoped lang="ts">
+
+</style>
