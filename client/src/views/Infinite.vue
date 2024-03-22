ba69fd9900417744f5f30410e5e894785ab95ebe
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-22 13:44:58 +0100

    Fully functioning minigames

--- client/src/views/Infinite.vue
+++ client/src/views/Infinite.vue
@@ -7,7 +7,7 @@
                     <Saxophone class="mt-4 justify-self-center" @play-note="playNote()" @check-note="checkNote()" v-if="setupComplete" />
                     <h1 v-if="setupComplete" class="text-light">Score: {{ score }}</h1>
                     <h2 v-if="setupComplete" class="text-light">{{ message }}</h2>
-                    <TogglePiano @note-played-by-player="notePlayed" class="mt-4" v-if="setupComplete" />
+                    <TogglePiano ref="togglePiano" @note-played-by-player="notePlayed" class="mt-4" v-if="setupComplete" />
                 </div>
             </div>
         </div>
@@ -27,7 +27,17 @@ export default defineComponent({
     data() {
         return {
             // Define an array with every possible note
-            notes: [['C1'], ['D1'], ['E1'], ['F1'], ['G1'], ['A1'], ['B1'], ['C2'], ['D2'], ['E2'], ['F2'], ['G2'], ['A2'], ['B2'], ['C3'], ['D3'], ['E3'], ['F3'], ['G3'], ['A3'], ['B3'], ['C4'], ['D4'], ['E4'], ['F4'], ['G4'], ['A4'], ['B4'], ['C5'], ['D5'], ['E5'], ['F5'], ['G5'], ['A5'], ['B5'], ['C6'], ['D6'], ['E6'], ['F6'], ['G6'], ['A6'], ['B6'], ['C7'], ['D7'], ['E7'], ['F7'], ['G7'], ['A7'], ['B7'], ['C8'], ['C1', 'Eb', 'G'], ['C#', 'E', 'G#'], ['Db', 'Fb', 'Ab'], ['D', 'F', 'A'], ['D#', 'F#', 'A#'], ['Eb', 'Gb', 'Bb'], ['E', 'G', 'B'], ['F', 'Ab', 'C'], ['F#', 'A', 'C#'], ['Gb', 'A', 'Db'], ['G', 'Bb', 'D'], ['G#', 'B', 'D#'], ['Ab', 'Cb', 'Eb'], ['A', 'C', 'E'], ['A#', 'C#', 'E#'], ['Bb', 'Db', 'F'], ['B', 'D', 'F#']],
+            // Only define sharp notes, flat notes are not accepted and will cause errors.
+            notes: [
+                ['C1'], ['C#1'], ['D1'], ['D#1'], ['E1'], ['F1'], ['F#1'], ['G1'], ['G#1'], ['A1'], ['A#1'], ['B1'],
+                ['C2'], ['C#2'], ['D2'], ['D#2'], ['E2'], ['F2'], ['F#2'], ['G2'], ['G#2'], ['A2'], ['A#2'], ['B2'],
+                ['C3'], ['C#3'], ['D3'], ['D#3'], ['E3'], ['F3'], ['F#3'], ['G3'], ['G#3'], ['A3'], ['A#3'], ['B3'],
+                ['C4'], ['C#4'], ['D4'], ['D#4'], ['E4'], ['F4'], ['F#4'], ['G4'], ['G#4'], ['A4'], ['A#4'], ['B4'],
+                ['C5'], ['C#5'], ['D5'], ['D#5'], ['E5'], ['F5'], ['F#5'], ['G5'], ['G#5'], ['A5'], ['A#5'], ['B5'],
+                ['C6'], ['C#6'], ['D6'], ['D#6'], ['E6'], ['F6'], ['F#6'], ['G6'], ['G#6'], ['A6'], ['A#6'], ['B6'],
+                ['C7'], ['C#7'], ['D7'], ['D#7'], ['E7'], ['F7'], ['F#7'], ['G7'], ['G#7'], ['A7'], ['A#7'], ['B7'],
+                ['C8'], ['C#8'], ['D8'], ['D#8'], ['E8'], ['F8'], ['F#8'], ['G8'], ['G#8'], ['A8'], ['A#8'], ['B8']
+            ],
             setupComplete: false,
             score: 0,
             bestScore: 0,
@@ -36,7 +46,7 @@ export default defineComponent({
             // Define an array to store the notes currently selected by the user
             selectedNote: [] as string[],
             message: "",
-            positiveMessages: ["Good", "Good good", "Very nice", "Damn", "Continue like this", "What an hear", "Whow, you impressed me"],
+            positiveMessages: ["Good", "Good good", "Very nice", "Damn", "Keep going like this", "What a ear", "Wow, you impressed me"],
         };
     },
     methods: {
@@ -72,7 +82,7 @@ export default defineComponent({
             }).toDestination();
 
             Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(['C1', 'Eb', 'G'], 3);
+                sampler.triggerAttackRelease(this.toGuessNote, 3);
             })
         },
         
@@ -101,7 +111,8 @@ export default defineComponent({
             // Reset the environment
             this.selectedNote = [];
             this.toGuessNote = [];
-            SetupInfinite.reset();
+            // Resets the component's array when notes are checked
+            (this.$refs.togglePiano as any).reset();
         },
         
         /**
--- client/src/views/SetOfNotes.vue
+++ client/src/views/SetOfNotes.vue
@@ -2,13 +2,17 @@
     <div class="container-fluid">
         <div class="row mt-5">
             <div class="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
-                <SetupSetOfNotes @setup-complete="setFilters" v-if="!setupComplete" />
-                <div class="game-wrapper d-flex flex-column justify-content-center text-center">
-                    <Saxophone class="mt-4" @play-note="playNote()" @check-note="checkNote(); " v-if="setupComplete" />
-                    <h1 v-if="setupComplete" class="text-light">Turn: {{ currentExerciseNumber }}/{{ exerciseNumber }}</h1>
-                    <h2 v-if="setupComplete" class="text-light">{{ message }}</h2>
-                    <TogglePiano ref="togglePiano" @note-played-by-player="notePlayed" class="mt-4" v-if="setupComplete" />
+                <Transition name="fade" mode="in-out">
+                    <SetupSetOfNotes @setup-complete="setFilters" v-if="!setupComplete" />
+                </Transition>
+                <transition name="fade" mode="in-out">
+                <div class="game-wrapper d-flex flex-column justify-content-center text-center" key="gameWrapper" v-if="setupComplete">
+                    <Saxophone class="mt-4" @play-note="playNote()" @check-note="checkNote(); " />
+                    <h1 class="text-light">Turn: {{ currentExerciseNumber }}/{{ exerciseNumber }}</h1>
+                    <h2 class="text-light">{{ message }}</h2>
+                    <TogglePiano ref="togglePiano" @note-played-by-player="notePlayed" class="mt-4" />
                 </div>
+                </transition>
             </div>
         </div>
     </div>
@@ -33,9 +37,56 @@ export default defineComponent({
     data() {
         return {
             // Define a dictionary with the notes divided by category
+            // Flat notes are not accepted, only write sharp notes.
             notes: {
-                "normal notes": [['C1'], ['D1'], ['E1'], ['F1'], ['G1'], ['A1'], ['B1'], ['C2'], ['D2'], ['E2'], ['F2'], ['G2'], ['A2'], ['B2'], ['C3'], ['D3'], ['E3'], ['F3'], ['G3'], ['A3'], ['B3'], ['C4'], ['D4'], ['E4'], ['F4'], ['G4'], ['A4'], ['B4'], ['C5'], ['D5'], ['E5'], ['F5'], ['G5'], ['A5'], ['B5'], ['C6'], ['D6'], ['E6'], ['F6'], ['G6'], ['A6'], ['B6'], ['C7'], ['D7'], ['E7'], ['F7'], ['G7'], ['A7'], ['B7'], ['C8']],
-                "minor chords": [['C1', 'Eb', 'G'], ['C#', 'E', 'G#'], ['Db', 'Fb', 'Ab'], ['D', 'F', 'A'], ['D#', 'F#', 'A#'], ['Eb', 'Gb', 'Bb'], ['E', 'G', 'B'], ['F', 'Ab', 'C'], ['F#', 'A', 'C#'], ['Gb', 'A', 'Db'], ['G', 'Bb', 'D'], ['G#', 'B', 'D#'], ['Ab', 'Cb', 'Eb'], ['A', 'C', 'E'], ['A#', 'C#', 'E#'], ['Bb', 'Db', 'F'], ['B', 'D', 'F#']]
+                "normal notes": [
+                    ['C1'], ['C#1'], ['D#1'], ['E1'], ['F#1'], ['G#1'], ['A#1'], ['C2'], ['C#2'], 
+                    ['D#2'], ['F2'], ['F#2'], ['G#2'], ['A#2'], ['C3'], ['C#3'], ['D#3'], ['F3'], 
+                    ['F#3'], ['G#3'], ['A#3'], ['C4'], ['C#4'], ['D#4'], ['F4'], ['F#4'], ['G#4'], 
+                    ['A#4'], ['C5'], ['C#5'], ['D#5'], ['F5'], ['F#5'], ['G#5'], ['A#5'], ['C6'], 
+                    ['C#6'], ['D#6'], ['F6'], ['F#6'], ['G#6'], ['A#6'], ['C7'], ['C#7'], ['D#7'], 
+                    ['F7'], ['F#7'], ['G#7'], ['A#7'], ['C8']],
+                "minor chords": [
+                    ['C1', 'D#1', 'G1'], ['C#2', 'E2', 'G#2'], ['C#2', 'E1', 'G#1'], ['D2', 'F2', 'A2'], 
+                    ['D#3', 'F#3', 'A#3'], ['D#3', 'F#3', 'A#3'], ['E3', 'G3', 'B3'], ['F3', 'G#3', 'C4'], 
+                    ['F#4', 'A4', 'C#5'], ['F#4', 'A4', 'C#4'], ['G4', 'A#4', 'D5'], ['G#5', 'B5', 'D#6'], 
+                    ['G#4', 'B3', 'D#4'], ['A4', 'C5', 'E5'], ['A#5', 'C#6', 'F6'], ['A#5', 'C#6', 'F6'], 
+                    ['B5', 'D6', 'F#6']],
+                "seventh chords": [
+                    ['C1', 'E1', 'G1', 'A#1'], ['C#2', 'F2', 'G#2', 'B2'], ['C#2', 'F2', 'G#2', 'B1'], 
+                    ['D2', 'F#2', 'A2', 'C3'], ['D#3', 'G3', 'A#3', 'C#4'], ['D#3', 'G3', 'A#3', 'C#4'], 
+                    ['E3', 'G#3', 'B3', 'D4'], ['F3', 'A3', 'C4', 'D#4'], ['F#4', 'A#4', 'C#5', 'E5'], 
+                    ['F#4', 'A#4', 'C#5', 'E5'], ['G4', 'B4', 'D5', 'F5'], ['G#5', 'C5', 'D#6', 'F#6'], 
+                    ['G#5', 'C5', 'D#6', 'F#6'], ['A5', 'C#6', 'E6', 'G6'], ['A#6', 'D6', 'F7', 'G#7'], 
+                    ['A#6', 'D6', 'F7', 'G#7'], ['B6', 'D#7', 'F#7', 'A7']],
+                "extended chords": [
+                    ['C1', 'E1', 'G1', 'A#1', 'D2'], ['C#2', 'F2', 'G#2', 'B2', 'D#3'], 
+                    ['C#2', 'F2', 'G#2', 'B1', 'D#3'], ['D2', 'F#2', 'A2', 'C3', 'E3'], 
+                    ['D#3', 'G3', 'A#3', 'C#4', 'F4'], ['D#3', 'G3', 'A#3', 'C#4', 'F4'], 
+                    ['E3', 'G#3', 'B3', 'D4', 'F#4'], ['F3', 'A3', 'C4', 'D#4', 'G4'], 
+                    ['F#4', 'A#4', 'C#5', 'E5', 'G#5'], ['F#4', 'A#4', 'C#5', 'E5', 'G#5'], 
+                    ['G4', 'B4', 'D5', 'F5', 'A5'], ['G#5', 'C5', 'D#6', 'F#6', 'A#6'], 
+                    ['G#5', 'C5', 'D#6', 'F#6', 'A#6'], ['A5', 'C#6', 'E6', 'G6', 'B6'], 
+                    ['A#6', 'D6', 'F7', 'G#7', 'C7'], ['A#6', 'D6', 'F7', 'G#7', 'C7'], 
+                    ['B6', 'D#7', 'F#7', 'A7', 'C#8']],
+                "augmented chords": [
+                    ['C1', 'E1', 'G#1'], ['C#2', 'F2', 'A2'], ['C#2', 'F2', 'A2'], ['D2', 'F#2', 'A#2'], 
+                    ['D#3', 'G3', 'B3'], ['D#3', 'G3', 'B3'], ['E3', 'G#3', 'C4'], ['F3', 'A3', 'C#4'], 
+                    ['F#4', 'A#4', 'D5'], ['F#4', 'A#4', 'D5'], ['G4', 'B4', 'D#5'], ['G#5', 'C5', 'E6'], 
+                    ['G#5', 'C5', 'E6'], ['A5', 'C#6', 'F6'], ['A#6', 'D6', 'F#7'], ['A#6', 'D6', 'F#7'], 
+                    ['B6', 'D#7', 'G7']],
+                "diminished chords": [
+                    ['C1', 'D#1', 'F#1'], ['C#2', 'E2', 'G2'], ['C#2', 'E2', 'G2'], ['D2', 'F2', 'G#2'], 
+                    ['D#3', 'F#3', 'A3'], ['D#3', 'F#3', 'A3'], ['E3', 'G3', 'A#3'], ['F3', 'G#3', 'B3'], 
+                    ['F#4', 'A4', 'C5'], ['F#4', 'A4', 'C5'], ['G4', 'A#4', 'C#5'], ['G#5', 'B5', 'D6'], 
+                    ['G#5', 'B5', 'D6'], ['A5', 'C6', 'D#6'], ['A#6', 'C#7', 'E7'], ['A#6', 'C#7', 'E7'], 
+                    ['B6', 'D7', 'F7']],
+                "chords inversions": [
+                    ['C1', 'G1', 'E2'], ['C#2', 'G#2', 'F3'], ['C#2', 'G#2', 'F3'], ['D2', 'A2', 'F#3'], 
+                    ['D#3', 'A#3', 'G4'], ['D#3', 'A#3', 'G4'], ['E3', 'B3', 'G#4'], ['F3', 'C4', 'A4'], 
+                    ['F#4', 'C#5', 'A#5'], ['F#4', 'C#5', 'A#5'], ['G4', 'D5', 'B5'], ['G#5', 'D#6', 'C6'], 
+                    ['G#5', 'D#6', 'C6'], ['A5', 'E6', 'C#7'], ['A#6', 'F7', 'D7'], ['A#6', 'F7', 'D7'], 
+                    ['B6', 'F#7', 'D#8']]
             } as { [key: string]: string[][] },
             setupComplete: false,
             // Define an array to store the notes to guess chosen in the setup
@@ -162,5 +213,16 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+.fade-enter-active {
+    transition: opacity 0.5s ease;
+}
 
-</style>../components/SetupSetOfNotes.vue
+.fade-leave-active {
+    transition: opacity 0s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
