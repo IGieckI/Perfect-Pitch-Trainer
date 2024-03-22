99a7353aafabd719c414acf3c327ccb36215204e
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-22 12:14:00 +0100

    Stat bees are now controlled

---
+++ server/src/controllers/StatsController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { collections } from "../services/DatabaseService";
+import Stats from '../models/Stats';
+
+/**
+ * Get stats for a specific player from the database.
+ */
+export const getStats = async (req: Request, res: Response) => {
+    if (!collections.stats) {
+        res.status(500).send("Database connection not established.");
+        return;
+    }
+
+    const playerId: number = parseInt(req.params.id);
+
+    try {
+        const stat = await collections.stats.findOne({ player_id: playerId });
+
+        if (stat) {
+            const playerStat = new Stats(stat.player_id, stat.best_score_infinite, stat.average_infinite_accuracy);
+            res.status(200).send(playerStat);
+        } else {
+            res.status(404).send("No stats found for this player.");
+        }
+    } catch (error: any) {
+        res.status(500).send(`Error retrieving stats: ${error.message}`);
+    }
+};
--- server/src/routes/StatsRouter.ts
+++ server/src/routes/StatsRouter.ts
@@ -1,33 +1,9 @@
-import express, { Request, Response } from "express";
-import { collections } from "../services/DatabaseService";
-import Stats from "../models/Stats";
+import express from "express";
+import * as statsController from "../controllers/StatsController"
 
 export const statsRouter = express.Router();
 statsRouter.use(express.json());
 
-/**
- * Get stats for a specific player from the database.
- */
-statsRouter.get("/:id", async (req: Request, res: Response) => {
-    if (!collections.stats) {
-        res.status(500).send("Database connection not established.");
-        return;
-    }
-
-    const playerId: number = parseInt(req.params.id);
-
-    try {
-        const stat = await collections.stats.findOne({ player_id: playerId });
-
-        if (stat) {
-            const playerStat = new Stats(stat.player_id, stat.best_score_infinite, stat.average_infinite_accuracy);
-            res.status(200).send(playerStat);
-        } else {
-            res.status(404).send("No stats found for this player.");
-        }
-    } catch (error: any) {
-        res.status(500).send(`Error retrieving stats: ${error.message}`);
-    }
-});
+statsRouter.get("/:id", statsController.getStats);
 
 export default statsRouter;
