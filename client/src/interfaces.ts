47f3269edced6e04f6f378f6229736361bf21f39
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-04-05 14:48:07 +0200

    Add API post calls

---
+++ client/src/interfaces.ts
@@ -0,0 +1,11 @@
+export interface SetOfNotesGame {
+    player_id: number,
+    n_turns: number,
+    n_categories: number,
+    n_correct: number,
+}
+
+export interface InfiniteGame {
+    player_id: number,
+    score: number,
+}
